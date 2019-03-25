4b19a0ac480a4c63b5bc20ddf03840f056748895
Michael Shea <MichaelShea1814@gmail.com>
2019-03-25 14:31:17 -0500

    added google maps to dashboard

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+    var map;
+
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 38.8781, lng: -97.6298},
+        zoom: 4.3
+      });
+    }
