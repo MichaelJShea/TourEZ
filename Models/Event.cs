b23d33c994449ccddd8928623bd7c73ea944352e
JBenedik <jeremybenedik@gmail.com>
2019-03-27 11:48:56 -0500

    updated event model

--- Models/Event.cs
+++ Models/Event.cs
@@ -5,13 +5,64 @@ using System.Collections.Generic;
 
 namespace TourEZ.Models
 {
+    public class ValidateDate: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            Event activity = (Event)validationContext.ObjectInstance;
+            DateTime now = DateTime.Now;
+            DateTime fullTime = activity.startDate.Date + activity.startTime.TimeOfDay;
+            if (fullTime > now)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Has to be in the future rapscallion.");
+            }
+        }
+    }
     public class Event
     {
         [Key]
         public int eventId {get;set;}
         public string eventName {get;set;}
-        public DateTime start {get;set;}
-        public DateTime end {get;set;}
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true), Display(Name = "Event Date")]
+        public DateTime startDate {get;set;}
+
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true), Display(Name = "Event Time")]
+        public DateTime startTime {get;set;}
+        public DateTime start
+        {
+            get
+            {
+                return startDate.Date + startTime.TimeOfDay;
+            }
+        }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true), Display(Name = "Event Date")]
+        public DateTime endDate {get;set;}
+
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true), Display(Name = "Event Time")]
+        public DateTime endTime {get;set;}
+        public DateTime end
+        {
+            get
+            {
+                return endDate.Date + endTime.TimeOfDay;
+            }
+        }
+        public TimeSpan duration
+        {
+            get 
+            {
+                return end - start;
+            }
+        }
         public string location {get;set;}
         public DateTime created_at {get;set;} = DateTime.Now;
 
